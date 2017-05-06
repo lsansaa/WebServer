6e9061c638159eb5ccc4d2a424f406a0410cba60
Luis San Martín <lsansaa@gmail.com>
2017-05-05 22:00:30 -0300

    Firts commit

--- src/main/java/ProcessRequestRunnable.java
+++ src/main/java/ProcessRequestRunnable.java
@@ -7,9 +7,9 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.net.URLDecoder;
 import java.nio.charset.Charset;
 
 /**
@@ -27,7 +27,6 @@ public class ProcessRequestRunnable implements Runnable {
      * Socket asociado al cliente.
      */
     private Socket socket;
-
 
     /**
      * Constructor
@@ -58,12 +57,6 @@ public class ProcessRequestRunnable implements Runnable {
 
         log.debug("Connection from {} in port {}.", socket.getInetAddress(), socket.getPort());
 
-        // A dormir por un segundo.
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
         try {
             processRequest(this.socket);
@@ -91,8 +84,10 @@ public class ProcessRequestRunnable implements Runnable {
         // Iterador de la peticion
         final LineIterator lineIterator = IOUtils.lineIterator(socket.getInputStream(), Charset.defaultCharset());
 
+        //Buffered Reader
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         // Peticion
-        final String request = getRequest(lineIterator);
+        final String request = getRequest(bufferedReader);
         log.debug("Request detected: {}", request);
 
         // Output
@@ -103,13 +98,35 @@ public class ProcessRequestRunnable implements Runnable {
         // HTTP header
         writeHeader(outputStream);
 
-        // HTTP Body
-        writeBody(outputStream, request);
+        // Dividir el request
 
+        final String[] request3 = StringUtils.split(request, " ");
+
+        //Cada componente
+        final String verb = request3[0];
+        final String file = request3[1];
+        final String version = request3[2];
+
+        //Si es el chat
+        if(StringUtils.startsWith(file, "/chat")){
+            if(StringUtils.contains(verb, "post")||StringUtils.contains(verb, "POST")){
+                final String payload = request3[3];
+                writeChat(outputStream, verb, file, payload);
+            }else{
+                writeChat(outputStream, verb, file, "");
+            }
+        }else{
+            // HTTP Body
+            writeBody(outputStream, request);
+        }
+        //cierro el buffered reader
+        //bufferedReader.close();
         // Cierro el stream
         IOUtils.closeQuietly(outputStream);
 
     }
+
+
 
     /**
      * Obtengo la linea de peticion de request.
@@ -140,9 +157,43 @@ public class ProcessRequestRunnable implements Runnable {
 
         }
 
+
+
+
         return request;
     }
 
+    private static String getRequest(BufferedReader bufferedReader)throws IOException{
+        String request = null;
+        int n = 0;
+
+        //code to read and print headers
+        String headerLine = null;
+        while((headerLine = bufferedReader.readLine()).length() != 0){
+            log.debug("Line {}: {}", ++n, headerLine);
+
+            // Guardo la peticion de la primera linea
+            if (n == 1) {
+                request = headerLine;
+            }
+            // Termine la peticion si llegue al final de la peticion
+            if (StringUtils.isEmpty(headerLine)) {
+                break;
+            }
+
+        }
+
+        //code to read the post payload data
+        StringBuilder payload = new StringBuilder();
+        while(bufferedReader.ready()){
+            payload.append((char) bufferedReader.read());
+        }
+        log.debug("Payload data is: {}", payload);
+
+        request =  request+" "+payload.toString();
+
+        return request;
+    }
     /**
      * Escribe el encabezado del protocolo HTTP.
      *
@@ -179,5 +230,62 @@ public class ProcessRequestRunnable implements Runnable {
 
     }
 
+    private static void writeChat(OutputStream outputStream, String verb, String file, String payload) throws IOException {
+        // archivo con la pagina principal del chat
+        FileInputStream indexFile =  new FileInputStream(new File("C:\\Projects\\02.WebServer.Multithread\\WebServer\\src\\main\\resources\\index.html"));
+        // obtener cuerpo de la pagina a partir del archivo
+        final String indexBody = IOUtils.toString(indexFile, "UTF-8");
+        //agregar ultimo mensaje al chat
+        AddToChatLog(payload);
+        //construir y obtener chat
+        final String chatLog =  getChatLog();
+        //construir body
+        final String result = StringUtils.replace(indexBody, "<!--ChatLog-->", chatLog);
+        IOUtils.write(result, outputStream, Charset.defaultCharset());
+    }
+    private static String getChatLog()throws IOException{
+        String chatBody = "";
+        // archivo con historial del chat
+        FileReader historyFile =  new FileReader("C:\\Projects\\02.WebServer.Multithread\\WebServer\\src\\main\\resources\\chatLog.txt");
+        BufferedReader bReader = new BufferedReader(historyFile);
+        String line;
+        int lineCounter = 0;
+        while ((line = bReader.readLine())!=null){
+            String userMsg = line;
+            if(userMsg!=null&&lineCounter>0) {
+                chatBody = chatBody
+                        +"<div class='bubble-container'><span class='bubble'><img class='bubble-avatar' src='' /><div class='bubble-text'><p>"
+                        +line
+                        +"</p></div><span class='bubble-quote' /></span></div>";
+
+            }
+            lineCounter++;
+        }
+
+
+
+        return chatBody;
+
+    }
+    private static void AddToChatLog(String file) throws IOException{
+        if(file!=null&&file!=""){
+            String [] fileSplited = StringUtils.split(file,"&");
+            if(fileSplited.length>1){
+                String [] msg = StringUtils.split(fileSplited[1],"=");
+                String message = URLDecoder.decode(msg[1], Charset.defaultCharset().name());
+                String user = URLDecoder.decode(StringUtils.split(fileSplited[0],"=")[1], Charset.defaultCharset().name());
+                File historyFile = new File ("C:\\Projects\\02.WebServer.Multithread\\WebServer\\src\\main\\resources\\chatLog.txt");
+                // archivo con historial del chat
+                FileInputStream historyFileStream =  new FileInputStream(historyFile);
+                final String historyLog = IOUtils.toString(historyFileStream, "UTF-8");
+                String newHistoryLog = historyLog+"\n"+user+" : "+message;
+                FileOutputStream newHistoryFile = new FileOutputStream(historyFile);
+                byte[] newHistoryLogBytes = newHistoryLog.getBytes();
+                newHistoryFile.write(newHistoryLogBytes);
+                newHistoryFile.flush();
+                newHistoryFile.close();
+            }
+        }
+    }
 
 }
